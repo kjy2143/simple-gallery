a9d7a0451c39e329a380e45ce10df27137c4ab17
kjy2143 <kjy2143@gmail.com>
2026-05-06 12:49:39 +0900

    fix: use import type for Photo in PhotoCard test

--- src/components/PhotoCard.test.tsx
+++ src/components/PhotoCard.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import { describe, it, expect } from 'vitest'
 import { PhotoCard } from './PhotoCard'
-import { Photo } from '../types/photo'
+import type { Photo } from '../types/photo'
 
 const mockPhoto: Photo = {
   albumId: 3,
