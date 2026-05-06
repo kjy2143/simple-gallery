0c96ae2f86194df2b04638463a3f643fe92ebbe5
kjy2143 <kjy2143@gmail.com>
2026-05-06 12:44:52 +0900

    feat: add PhotoGrid component

---
+++ src/components/PhotoGrid.tsx
@@ -0,0 +1,16 @@
+import { Photo } from '../types/photo'
+import { PhotoCard } from './PhotoCard'
+
+interface PhotoGridProps {
+  photos: Photo[]
+}
+
+export function PhotoGrid({ photos }: PhotoGridProps) {
+  return (
+    <div className="grid grid-cols-2 gap-3 p-3">
+      {photos.map(photo => (
+        <PhotoCard key={photo.id} photo={photo} />
+      ))}
+    </div>
+  )
+}
